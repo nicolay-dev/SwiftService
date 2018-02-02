c2026dfa0a195a6d7ba84488df9c2b2850a6bdf7
Nicolay <Nicolay.JG@outlook.com>
2018-02-02 13:19:40 -0500

    Lista de Menus

--- apps/menu/views.py
+++ apps/menu/views.py
@@ -8,13 +8,18 @@ class MenuListView(ListView):
     model = Tipo
     template_name = 'menu/menuList.html'
 
-    def __init__(self, *args, **kwargs):
-        import pdb;pdb.set_trace()
-        nico = super(MenuListView, self).get_context_data(**kwargs)
-        self.restaurante = Restaurante.objects.get(slug=self.kwargs.get('slug'))
-        return
+    # def __init__(self,**kwargs):
+    #     self.restaurante = super(MenuListView, self).get_context_data(**kwargs)
+    #     pass
 
-    def get_queryset(self):
+    # def get(self, request, *args, **kwargs):
+    #     self.restaurante = Restaurante.objects.get(
+    #         slug=kwargs['slug'])
+    #     pass
+
+    def get_queryset(self, *args, **kwargs):
+        # import pdb;pdb.set_trace()
+        self.restaurante = Restaurante.objects.get(slug=self.kwargs['slug'])
         return Tipo.objects.filter(
             restaurante__slug=self.restaurante.slug)
 
@@ -29,7 +34,7 @@ class MenuListView(ListView):
         context['mesa_id'] = self.kwargs.get('mesa_id')
         context['restaurante_slug'] = self.restaurante.slug
         context['restaurante_nombre'] = self.restaurante.nombre
-        context['restaurante_desc'] = self.restaurante.slug
+        context['restaurante_desc'] = self.restaurante.descMenu
         context['object_list'] = Platillo.objects.filter(
             restaurante__slug=self.restaurante.slug)
         return context
