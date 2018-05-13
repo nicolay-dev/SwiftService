fca59138d987e9d5cd2227a54a859c8b2808ca76
Nicolay <Nicolay.JG@outlook.com>
2018-05-13 07:19:36 -0500

    cambio de modelo Usuario a User
    hay dos tablas de Usuario, como django ya vienen con una libreira uq maneja los usuarios mejor usar esa...

--- apps/restaurant/views.py
+++ apps/restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from apps.restaurant.models import Usuario
 from apps.restaurant.forms import UserForm
 from django.urls import reverse_lazy
@@ -7,7 +9,7 @@ from django.urls import reverse_lazy
 
 
 class CreateUser(CreateView):
-    model = Usuario
-    form_class = UserForm
+    model = User
+    form_class = UserCreationForm
     template_name = 'manager/menu_form.html'
     success_url = reverse_lazy('Manager:index_manager')
